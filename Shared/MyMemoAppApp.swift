2321869f64b38df737fdee8df4b117c9859a7f62
YunHyeok <djbyh645047@naver.com>
2022-07-04 16:25:56 +0900

    Initial Commit

---
+++ Shared/MyMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMemoAppApp.swift
+//  Shared
+//
+//  Created by 배윤혁 on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct MyMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
