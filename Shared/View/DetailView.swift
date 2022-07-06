3e3578ad76f76e8c0337a0e0724e3b2e68d00c86
YunHyeok <djbyh645047@naver.com>
2022-07-06 12:31:32 +0900

    commit3

--- Shared/View/DetailView.swift
+++ Shared/View/DetailView.swift
@@ -8,28 +8,24 @@
 import SwiftUI
 
 struct DetailView: View {
-    // 1.
-    @State private var text: String = ""
-    
+    @State private var TextEditor: String = " "
+
     var body: some View {
-        VStack {
-            // 2.
-            TextEditor(text: $text)
-                .padding()
-                .foregroundColor(Color.black)
-                .font(.custom("원하는글꼴", size: 20))
-                .lineSpacing(5) //줄 간격
-    
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 800)
-                .border(Color.yellow, width: 1)
-    
-            Spacer()
+        NavigationView {
+            VStack { 
+                SwiftUI.TextEditor(text: $TextEditor)
+                Button(action: {
+                    
+                }, label: {
+                    Text("Save")
+                })
+            }
         }
     }
 }
+        
 
-
-struct DetailView_Previews: PreviewProvider {
+struct O: PreviewProvider {
     static var previews: some View {
         DetailView()
     }
---
+++ Shared/View/savebutton.swift
@@ -0,0 +1,20 @@
+//
+//  savebutton.swift
+//  MyMemoApp
+//
+//  Created by 배윤혁 on 2022/07/05.
+//
+
+import SwiftUI
+
+struct savebutton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct savebutton_Previews: PreviewProvider {
+    static var previews: some View {
+        savebutton()
+    }
+}
